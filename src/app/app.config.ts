5853fc1d1a773d4da851813e2809b51b7fa11244
Rafal <rof16@wp.pl>
2025-05-28 15:02:17 +0200

    1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -18,7 +18,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-
+    
     importProvidersFrom([
       AngularFirestoreModule,
       AngularFireModule.initializeApp(environment.firebaseConfig),
